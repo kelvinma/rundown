80a39212d31f408f464732078d9f56b145e28fa6
kelvinma <me@kelvinma.com>
2015-12-14 15:26:07 -0500

    Created application adapter and added properties to athlete model

--- app/athlete/model.js
+++ app/athlete/model.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+  first_name: DS.attr('string'),
+  last_name: DS.attr('string'),
+  gender: DS.attr('string'),
+  workouts: DS.hasMany('workout', {async:true})
 });
